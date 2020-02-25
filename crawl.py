8fd1fc4cb50ed694e7cb8a39af79d4b3fcba98b6
oGsLP <1145234011@qq.com>
2020-02-26 00:15:35 +0800

    :bookmark: add auto id-switch support，bump into v0.2.0

--- crawl.py
+++ crawl.py
@@ -15,7 +15,7 @@ from bs4 import BeautifulSoup
 import re
 
 profile_url = "https://live.kuaishou.com/profile/"
-page_url = "https://live.kuaishou.com/m_graphql"
+data_url = "https://live.kuaishou.com/m_graphql"
 work_url = "https://live.kuaishou.com/u/"
 
 param_did = "?did=web_67063b98a8884a80dccf7ed18172acfa"
@@ -31,7 +31,7 @@ headers = {
     'Sec-Fetch-Mode': 'cors',
     'Sec-Fetch-Site': 'same-origin',
 
-    # User-Agent 根据自己的电脑修改
+    # User-Agent/Cookie 根据自己的电脑修改
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36',
     'Cookie': 'did=web_67063b98a8884a80dccf7ed18172acfa; clientid=3; client_key=65890b29; didv=1579495215536; userId=1489161552; kuaishou.live.bfb1s=477cb0011daca84b36b3a4676857e5a1; userId=1489161552; kuaishou.live.web_st=ChRrdWFpc2hvdS5saXZlLndlYi5zdBKgATq2nZA1Yla0gW-XmZNyzWqZVVbHQxzEi4IPFxnLR6lwBKiJQPYjnwHvU5p2ejU_456SQwWYx5AZX-X-gqKVvrObQ6mNLYmMTu9O7WYvj8-LBDTNnkLpQ7tpB3Yvumc-HPsnZkiKtWZkz54OdwENQaQ8OHMs-SZK-pIA77xXYv-CKPaW_40TnMRwaD4_TVcci5_ADYx8KlP76uPvkbh6TEQaEpwewD8CakIZrC7t8JEKqqml3iIgqvGUkWi91EPRqyOxbgLP49WtVMpVGdQYBobIs4upgr0oBTAB; kuaishou.live.web_ph=a70df58d8d097d3b613ace8ca7b6c502e8b1'
 }
@@ -42,7 +42,7 @@ def crawl_user(uid):
     payload = {"operationName": "privateFeedsQuery",
                "variables": {"principalId": uid, "pcursor": "", "count": 999},
                "query": "query privateFeedsQuery($principalId: String, $pcursor: String, $count: Int) {\n  privateFeeds(principalId: $principalId, pcursor: $pcursor, count: $count) {\n    pcursor\n    list {\n      id\n      thumbnailUrl\n      poster\n      workType\n      type\n      useVideoPlayer\n      imgUrls\n      imgSizes\n      magicFace\n      musicName\n      caption\n      location\n      liked\n      onlyFollowerCanComment\n      relativeHeight\n      timestamp\n      width\n      height\n      counts {\n        displayView\n        displayLike\n        displayComment\n        __typename\n      }\n      user {\n        id\n        eid\n        name\n        avatar\n        __typename\n      }\n      expTag\n      __typename\n    }\n    __typename\n  }\n}\n"}
-    res = requests.post(page_url, headers=headers, json=payload)
+    res = requests.post(data_url, headers=headers, json=payload)
 
     works = json.loads(res.content.decode(encoding='utf-8', errors='strict'))['data']['privateFeeds']['list']
 
@@ -146,9 +146,21 @@ def read_preset():
     return u_arr
 
 
+def switch_id(uid):
+    payload = {"operationName": "SearchOverviewQuery",
+               "variables": {"keyword": uid, "ussid": None},
+               "query": "query SearchOverviewQuery($keyword: String, $ussid: String) {\n  pcSearchOverview(keyword: $keyword, ussid: $ussid) {\n    list {\n      ... on SearchCategoryList {\n        type\n        list {\n          categoryId\n          categoryAbbr\n          title\n          src\n          __typename\n        }\n        __typename\n      }\n      ... on SearchUserList {\n        type\n        ussid\n        list {\n          id\n          name\n          living\n          avatar\n          sex\n          description\n          counts {\n            fan\n            follow\n            photo\n            __typename\n          }\n          __typename\n        }\n        __typename\n      }\n      ... on SearchLivestreamList {\n        type\n        lssid\n        list {\n          user {\n            id\n            avatar\n            name\n            __typename\n          }\n          poster\n          coverUrl\n          caption\n          id\n          playUrls {\n            quality\n            url\n            __typename\n          }\n          quality\n          gameInfo {\n            category\n            name\n            pubgSurvival\n            type\n            kingHero\n            __typename\n          }\n          hasRedPack\n          liveGuess\n          expTag\n          __typename\n        }\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n}\n"}
+    res = requests.post(data_url, headers=headers, json=payload)
+    dt = json.loads(res.content.decode(encoding='utf-8', errors='strict'))['data']
+    return dt['pcSearchOverview']['list'][1]['list'][0]['id']
+
+
 def crawl():
     for uid in read_preset():
-        crawl_user(uid)
+        if uid.isdigit():
+            crawl_user(switch_id(uid))
+        else:
+            crawl_user(uid)
 
 
 if __name__ == "__main__":
