54371b1a0af4fd2fc8b4f2fd38d12c64b26d968d
oGsLP <1145234011@qq.com>
2020-08-06 01:59:43 +0800

    Feature v0.5: modify noMark url for video, how to get did_web to be done

--- lib/crawler.py
+++ lib/crawler.py
@@ -22,7 +22,7 @@ INFO = {
 
 PROFILE_URL = "https://live.kuaishou.com/profile/"
 DATA_URL = "https://live.kuaishou.com/m_graphql"
-WORK_URL = "https://v.kuaishou.com/fw/photo/"
+WORK_URL = "https://m.gifshow.com/fw/photo/"
 
 
 class Crawler:
@@ -61,6 +61,8 @@ class Crawler:
     def set_did(self, did):
         self.__param_did = did
         self.__headers_web['Cookie'] = 'did=' + did + "; userId="
+        # todo figure out how to get this did_web
+        self.__headers_mobile['Cookie'] = 'did=' + 'web_1ab324e23bde4e20a8082b38a1c0bc81'
 
     def crawl(self):
         print("准备开始爬取，共有%d个用户..." % len(self.__crawl_list))
@@ -109,7 +111,7 @@ class Crawler:
         print(" 共有" + str(len(works)) + "个作品")
 
         for j in range(len(works)):
-            self.__crawl_work(uid, dir, works[j], j + 1)
+            self.__crawl_work(dir, works[j], j + 1)
             time.sleep(1)
 
         print("用户 " + name + "爬取完成!")
@@ -124,29 +126,30 @@ class Crawler:
      * 视频: `video` 需要解析html获得视频链接
     '''
 
-    def __crawl_work(self, uid, dir, work, wdx):
+    def __crawl_work(self, dir, work, wdx, like=False):
         w_type = work['workType']
         w_caption = re.sub(r"\s+", " ", work['caption'])
         w_name = re.sub(r'[\\/:*?"<>|\r\n]+', "", w_caption)[0:24]
         w_time = time.strftime('%Y-%m-%d', time.localtime(work['timestamp'] / 1000))
         w_index = ""
-        if self.__date_cache == w_time:
-            self.__date_count = self.__date_count + 1
-            if self.__date_count > 0:
-                w_index = "(%d)" % self.__date_count
-        else:
-            self.__date_cache = w_time
-            self.__date_count = 0
+        if not like:
+            if self.__date_cache == w_time:
+                self.__date_count = self.__date_count + 1
+                if self.__date_count > 0:
+                    w_index = "(%d)" % self.__date_count
+            else:
+                self.__date_cache = w_time
+                self.__date_count = 0
 
         if w_type == 'vertical' or w_type == 'multiple' or w_type == "single" or w_type == 'ksong':
             w_urls = work['imgUrls']
             l = len(w_urls)
             print("  " + str(wdx) + ")图集作品：" + w_caption + "，" + "共有" + str(l) + "张图片")
             for i in range(l):
-                p_name = w_time + w_index + "_" + w_name + "_" + str(i + 1)+'.jpg'
+                p_name = w_time + w_index + "_" + w_name + "_" + str(i + 1) + '.jpg'
                 pic = dir + p_name
                 if not os.path.exists(pic):
-                    r = requests.get(w_urls[i].replace("webp","jpg"))
+                    r = requests.get(w_urls[i].replace("webp", "jpg"))
                     r.raise_for_status()
                     with open(pic, "wb") as f:
                         f.write(r.content)
@@ -156,11 +159,15 @@ class Crawler:
         elif w_type == 'video':
             w_url = WORK_URL + work['id']
             res = requests.get(w_url, headers=self.__headers_mobile,
-                               params={"fid": 1841409882, "cc": "share_copylink", "shareId": "143108986354"})
+                               params={"did": self.__param_did})
             html = res.text
-            waitreplace = work['id'] + '&#34.*?&#34;srcNoMark&#34;:&#34;(.*?)&#34;'
+            # with open("data/" + work['id'] + ".html", "w") as fp:
+            #     fp.write(html)
+            pattern = '"srcNoMark":"(https:.*?).mp4'
 
-            v_url = re.findall(waitreplace, html)
+            v_url = re.search(pattern, html).group(1)+".mp4"
+
+            print(v_url)
             # pattern = re.compile(r"playUrl", re.MULTILINE | re.DOTALL)
             # script = soup.find("script", text=pattern)
             # s = pattern.search(script.text).string
@@ -174,7 +181,7 @@ class Crawler:
 
             if v_url:
                 if not os.path.exists(video):
-                    r = requests.get(v_url[0])
+                    r = requests.get(v_url)
                     r.raise_for_status()
 
                     with open(video, "wb") as f:
@@ -186,6 +193,20 @@ class Crawler:
                 print("未找到视频")
         else:
             print("错误的类型")
+
+    def crawl_like(self, uid):
+        payload = {"operationName": "likedFeedsQuery",
+                   "variables": {"principalId": uid, "pcursor": "", "count": 9999},
+                   "query": "query likedFeedsQuery($principalId: String, $pcursor: String, $count: Int) {\n  likedFeeds(principalId: $principalId, pcursor: $pcursor, count: $count) {\n    pcursor\n    list {\n      id\n      thumbnailUrl\n      poster\n      workType\n      type\n      useVideoPlayer\n      imgUrls\n      imgSizes\n      magicFace\n      musicName\n      caption\n      location\n      liked\n      onlyFollowerCanComment\n      relativeHeight\n      timestamp\n      width\n      height\n      counts {\n        displayView\n        displayLike\n        displayComment\n        __typename\n      }\n      user {\n        id\n        eid\n        name\n        avatar\n        __typename\n      }\n      expTag\n      __typename\n    }\n    __typename\n  }\n}\n"}
+        cookie_like = "live_deviceid=web_2761de01059f8b0a60555ae7ff5d69e4; client_key=65890b29; clientid=3; did=web_2761de01059f8b0a60555ae7ff5d69e4; didv=1583906306185; userId=734830893; Hm_lvt_86a27b7db2c5c0ae37fee4a8a35033ee=1585027806; kuaishou.live.bfb1s=ac5f27b3b62895859c4c1622f49856a4; userId=734830893; kuaishou.live.web_st=ChRrdWFpc2hvdS5saXZlLndlYi5zdBKgAVDawiXZPtyTC5VMgLpjQ3-Qbeq711iW5YSERJLxNu8GdpK4_sVV7noXOpmENiqNqOR33JD4y3KVH1I-DrCL2YAlluhKNcYx5aoAfZ1UPBDGvaOc26zb0y4Vlnozd394yASJJr8dx-18Rmh9lL2DeHS9ki5itZuKKWdXXvB3gfVmzKs4bv_9EH_GBVYMpXyX5H9fgdM6Rtw4wyzbPVrS_48aEurKvNghXkU0vIm_W_UXPUfuwSIgNlpIpBIpRlbM9q8mF6SBu_1NHP6ppeFPEtUrmAs0TMUoBTAB; kuaishou.live.web_ph=10695f787577fd9ccd02071217a8b1a73ad0"
+        self.__headers_web["Cookie"] = cookie_like
+        res = requests.post(DATA_URL, headers=self.__headers_web, json=payload)
+        dta = json.loads(res.content.decode(encoding='utf-8', errors='strict'))['data']['likedFeeds']
+        works = dta['list']
+        print("开始爬取用户 " + uid + " 的喜欢作品")
+        print(" 共有" + str(len(works)) + "个作品")
+        for i in range(len(works)):
+            self.__crawl_work("data/Liked/", works[i], i + 1, True)
 
     def __read_preset(self):
         p_path = "preset"
